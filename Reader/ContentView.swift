08633c712d968fb1753c8673e7377110244594e1
jsketcham <jsketcham@earthlink.net>
2026-04-29 11:42:43 -0700

    Initial Commit

---
+++ Reader/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Reader
+//
+//  Created by Jim on 4/29/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Reader/ReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReaderApp.swift
+//  Reader
+//
+//  Created by Jim on 4/29/26.
+//
+
+import SwiftUI
+
+@main
+struct ReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
